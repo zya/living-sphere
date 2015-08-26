bc59942d51864623bbeb70517988b28772cde9a5
Ehsan Ziya <ehsan.ziya@gmail.com>
2015-08-27 00:26:09 +0100

    Found the way to color each face of the geometry

--- lib/shaders/vertex.vert
+++ lib/shaders/vertex.vert
@@ -2,19 +2,41 @@ uniform float time;
 uniform float amp;
 uniform float val1;
 uniform float val2;
+uniform vec3 color1;
+uniform vec3 color2;
 
-float random(vec2 st) {
-    return fract(sin(dot(st.xy,vec2(12.9898,78.233)))* 43758.5453123);
+varying vec3 vColor;
+// varying vec3 vCoord;
+varying vec3 nColor;
+
+highp float rand(vec2 co) {
+    highp float a = 12.9898;
+    highp float b = 78.233;
+    highp float c = 43758.5453;
+    highp float dt= dot(co.xy ,vec2(a,b));
+    highp float sn= mod(dt,3.14);
+    return fract(sin(sn) * c);
 }
 
 void main() {
 
-  float sinedX = sin((time - position.x - val1) * 0.004) * 30.0 * random(position.xz) * 1.2;
-  float sinedY = cos((time - position.y + val2) * 0.002) * 20.0 * random(position.yz) * 1.8;
-  float sinedZ = sin((time - position.z + val2) * 0.0001) * 20.0 * random(position.yz) * 1.8;
-  vec3 p = vec3(position.x + (sinedX * amp), position.y + sinedY * amp, sinedZ * amp);
+  float sinedX = sin((time - position.x - val1) * 0.004) * 30.0 * rand(position.xz) * 1.2;
+  float sinedY = cos((time - position.y + val2) * 0.002) * 20.0 * rand(position.xz) * 1.8;
+  float sinedZ = sin((time - position.z + val2) * 0.0001) * 20.0 * rand(position.yz) * 1.8;
+  vec3 p = vec3(position.x + (sinedX * amp), position.y + sinedY * amp, position.z + sinedZ * amp);
 
-  gl_Position = projectionMatrix *
-                modelViewMatrix *
-                vec4(p, 1.0);
+  float f = mod(position.x, 2.0);
+  float f3 = mod(position.z, 8.0);
+  float f2 = mod(position.z, 2.0);
+
+  if( f == 0.0 || f2 == 0.0 || f3 == 0.0) {
+    vColor = color1;
+  } else {
+    vColor = color2;
+  }
+
+  nColor = color;
+
+  // vCoord = p;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(p, 1.0);
 }
--- main.js
+++ main.js
@@ -1,59 +1,64 @@
 var THREE = require('three');
+var OrbitControls = require('three-orbit-controls')(THREE);
 var dynamics = require('dynamics.js');
 var vs = require('./lib/shaders/vertex.vert')();
 var fs = require('./lib/shaders/fragment.frag')();
-var amp = 2.0;
-var val1 = 300;
-var val2 = 200;
+var _ = require('lodash');
+
+var amp = 0.5;
+var val1 = 100;
+var val2 = 50;
 var speed1 = 0.002;
 var speed2 = 0.004;
-var color = new THREE.Color(1.0, 0.2, 0.1);
+var color1 = new THREE.Color(1.0, 0.2, 0.1);
 var color2 = new THREE.Color(1.0, 0.2, 0.1);
 
 var scene = new THREE.Scene();
 
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
 camera.position.z = 1000;
+
 var startTime = Date.now();
 
-var geometry = new THREE.SphereGeometry(500, 200, 100);
-console.log(geometry.vertices.length);
+var geometry = new THREE.SphereGeometry(400, 200, 200);
 
-var m = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
-  wireframe: true
-});
+var uniforms = {
+  time: {
+    type: "f",
+    value: 1.0
+  },
+  amp: {
+    type: "f",
+    value: 1.0
+  },
+  val1: {
+    type: "f",
+    value: 300.0
+  },
+  val2: {
+    type: "f",
+    value: 200.0
+  },
+  color1: {
+    type: "c",
+    value: color1
+  },
+  color2: {
+    type: "c",
+    value: color2
+  }
+};
 
 var material = new THREE.ShaderMaterial({
   vertexShader: vs,
   fragmentShader: fs,
   wireframe: true,
-  uniforms: {
-    time: {
-      type: "f",
-      value: 1.0
-    },
-    amp: {
-      type: "f",
-      value: 1.0
-    },
-    val1: {
-      type: "f",
-      value: 300.0
-    },
-    val2: {
-      type: "f",
-      value: 200.0
-    },
-    color: {
-      type: "c",
-      value: color
-    },
-    color2: {
-      type: "c",
-      value: color2
-    }
-  },
+  uniforms: uniforms,
+  vertexColors: THREE.FaceColors
+});
+
+_.forEach(geometry.faces, function (face) {
+  face.color = new THREE.Color(Math.random(), Math.random(), Math.random());
 });
 
 var mesh = new THREE.Mesh(geometry, material);
@@ -65,23 +70,23 @@ var renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
+
+controls = new OrbitControls(camera, renderer.domElement);
+controls.userPan = false;
+controls.userPanSpeed = 0.0;
+controls.maxDistance = 5000.0;
+controls.maxPolarAngle = Math.PI * 0.495;
+// controls.center.set(0, 75, 0);
+
 window.amp = amp;
 window.val1 = val1;
 window.val2 = val2;
 
-document.addEventListener('click', function() {
+document.addEventListener('click', function () {
 
-  var random = Math.random() * 10 - 5;
-  dynamics.animate(window, {
-    amp: amp + random
-  }, {
-    type: dynamics.spring,
-    frequency: 400,
-    friction: 200,
-    duration: 2500
-  });
+  var random = (Math.random() * 1.5) - 0.75;
 
-  dynamics.animate(color, {
+  dynamics.animate(color1, {
     r: Math.random(),
     g: Math.random(),
     b: Math.random()
@@ -91,6 +96,7 @@ document.addEventListener('click', function() {
   });
 
   dynamics.animate(window, {
+    amp: window.amp + random,
     val1: Math.random() * 500,
     val2: Math.random() * 400
   }, {
@@ -106,11 +112,11 @@ function animate() {
   material.uniforms.amp.value = window.amp;
   material.uniforms.val1.value = window.val1;
   material.uniforms.val2.value = window.val2;
-  material.uniforms.color.value = color;
+  material.uniforms.color1.value = color1;
   material.uniforms.color2.value = color2;
-  // mesh.rotation.y += 0.0001;
-  // mesh.rotation.z += 0.00002;
-  // mesh.rotation.x += 0.00002;
+  // mesh.rotation.x += 0.002;
+  // mesh.rotation.y += 0.002;
+  // mesh.rotation.z += 0.002;
   renderer.render(scene, camera);
 }
 
